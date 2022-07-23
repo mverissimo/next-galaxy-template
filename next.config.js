dc264a325b8893f2dd209debc665dfc8ddfe8386
mverissimo <mverissimoo@hotmail.com>
2022-07-23 12:52:05 -0300

    feat(app): add markdoc and tailwind

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withMarkdoc = require('@markdoc/next.js');
+
+module.exports = withMarkdoc({
+  mode: 'static',
+})({
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = nextConfig;
+  pageExtensions: ['md', 'mdoc', 'ts', 'tsx'],
+});
